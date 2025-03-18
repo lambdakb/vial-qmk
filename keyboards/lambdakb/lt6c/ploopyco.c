b6b1887a096bc7a65c28efaa6be3f43c3b2aac18
JP Roemer <2822534+0rax@users.noreply.github.com>
2025-03-18 01:16:37 +0100

    Add LT6C base firmware

---
+++ keyboards/lambdakb/lt6c/ploopyco.c
@@ -0,0 +1,148 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2020 Ploopy Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ploopyco.h"
+#include "analog.h"
+
+// for legacy support
+#if defined(OPT_DEBOUNCE) && !defined(PLOOPY_SCROLL_DEBOUNCE)
+#    define PLOOPY_SCROLL_DEBOUNCE OPT_DEBOUNCE
+#endif
+#if defined(SCROLL_BUTT_DEBOUNCE) && !defined(PLOOPY_SCROLL_BUTTON_DEBOUNCE)
+#    define PLOOPY_SCROLL_BUTTON_DEBOUNCE SCROLL_BUTT_DEBOUNCE
+#endif
+
+#ifndef PLOOPY_SCROLL_DEBOUNCE
+#    define PLOOPY_SCROLL_DEBOUNCE 5
+#endif
+#ifndef PLOOPY_SCROLL_BUTTON_DEBOUNCE
+#    define PLOOPY_SCROLL_BUTTON_DEBOUNCE 100
+#endif
+
+#ifndef PLOOPY_DPI_OPTIONS
+#    define PLOOPY_DPI_OPTIONS \
+        { 600, 900, 1200, 1600, 2400 }
+#    ifndef PLOOPY_DPI_DEFAULT
+#        define PLOOPY_DPI_DEFAULT 1
+#    endif
+#endif
+#ifndef PLOOPY_DPI_DEFAULT
+#    define PLOOPY_DPI_DEFAULT 0
+#endif
+#ifndef PLOOPY_DRAGSCROLL_DIVISOR_H
+#    define PLOOPY_DRAGSCROLL_DIVISOR_H 8.0
+#endif
+#ifndef PLOOPY_DRAGSCROLL_DIVISOR_V
+#    define PLOOPY_DRAGSCROLL_DIVISOR_V 8.0
+#endif
+#ifndef ENCODER_BUTTON_ROW
+#    define ENCODER_BUTTON_ROW 0
+#endif
+#ifndef ENCODER_BUTTON_COL
+#    define ENCODER_BUTTON_COL 0
+#endif
+
+keyboard_config_t keyboard_config;
+uint16_t          dpi_array[] = PLOOPY_DPI_OPTIONS;
+#define DPI_OPTION_SIZE ARRAY_SIZE(dpi_array)
+
+// Trackball State
+bool  is_scroll_clicked    = false;
+bool  is_drag_scroll       = false;
+float scroll_accumulated_h = 0;
+float scroll_accumulated_v = 0;
+
+report_mouse_t pointing_device_task_kb(report_mouse_t mouse_report) {
+    if (is_drag_scroll) {
+        scroll_accumulated_h += (float)mouse_report.x / PLOOPY_DRAGSCROLL_DIVISOR_H;
+        scroll_accumulated_v += (float)mouse_report.y / PLOOPY_DRAGSCROLL_DIVISOR_V;
+
+        // Assign integer parts of accumulated scroll values to the mouse report
+        mouse_report.h = (int8_t)scroll_accumulated_h;
+#ifdef PLOOPY_DRAGSCROLL_INVERT
+        mouse_report.v = -(int8_t)scroll_accumulated_v;
+#else
+        mouse_report.v = (int8_t)scroll_accumulated_v;
+#endif
+
+        // Update accumulated scroll values by subtracting the integer parts
+        scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
+        scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
+
+        // Clear the X and Y values of the mouse report
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+    }
+
+    return pointing_device_task_user(mouse_report);
+}
+
+bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
+    if (debug_mouse) {
+        dprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
+    }
+
+    // Update Timer to prevent accidental scrolls
+#ifdef ENCODER_ENABLE
+    if ((record->event.key.col == ENCODER_BUTTON_COL) && (record->event.key.row == ENCODER_BUTTON_ROW)) {
+        lastMidClick      = timer_read();
+        is_scroll_clicked = record->event.pressed;
+    }
+#endif
+
+    if (!process_record_user(keycode, record)) {
+        return false;
+    }
+
+    if (keycode == DPI_CONFIG && record->event.pressed) {
+        keyboard_config.dpi_config = (keyboard_config.dpi_config + 1) % DPI_OPTION_SIZE;
+        eeconfig_update_kb(keyboard_config.raw);
+        pointing_device_set_cpi(dpi_array[keyboard_config.dpi_config]);
+    }
+
+    if (keycode == DRAG_SCROLL) {
+#ifdef PLOOPY_DRAGSCROLL_MOMENTARY
+        is_drag_scroll = record->event.pressed;
+#else
+        if (record->event.pressed) {
+            is_drag_scroll ^= 1;
+        }
+#endif
+    }
+
+    return true;
+}
+
+// Hardware Setup
+
+void pointing_device_init_kb(void) {
+    keyboard_config.raw = eeconfig_read_kb();
+    if (keyboard_config.dpi_config > DPI_OPTION_SIZE) {
+        eeconfig_init_kb();
+    }
+    pointing_device_set_cpi(dpi_array[keyboard_config.dpi_config]);
+}
+
+void eeconfig_init_kb(void) {
+    keyboard_config.dpi_config = PLOOPY_DPI_DEFAULT;
+    eeconfig_update_kb(keyboard_config.raw);
+    eeconfig_init_user();
+}
