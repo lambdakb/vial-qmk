42771715a148b3bc69ca8d98217d93028e353236
JP Roemer <jp@roemer.im>
2025-03-18 23:18:11 +0100

    LT6C Firmware ported from Ploopy Maromys (#1)
    * Add LT6C base firmware
    * Add custom keycodes + cleanup vial.json
    * Disable user LED
    * Remove encoder related thing from ploopyco lib
    * Update copyrights
    * Consolidate keyboard.json

---
+++ keyboards/lambdakb/lt6c/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFA, 0x99, 0xF4, 0x92, 0x56, 0x9E, 0x36, 0xD1}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 5 }
---
+++ keyboards/lambdakb/lt6c/lt6c.c
@@ -0,0 +1,21 @@
+// Copyright 2025 JP Roemer (@0rax)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "quantum.h"
+
+void keyboard_pre_init_kb(void) {
+    // Disable onboard user LED
+    gpio_set_pin_output(GP25);
+    gpio_write_pin_high(GP25);
+    gpio_set_pin_output(GP16);
+    gpio_write_pin_high(GP16);
+    gpio_set_pin_output(GP17);
+    gpio_write_pin_high(GP17);
+
+    // Enable onboard addressable RGB led (NeoPixel)
+    gpio_set_pin_output(GP11);
+    gpio_write_pin_high(GP11);
+
+    // Call user pre-init code
+    keyboard_pre_init_user();
+}
