0cb6e84dc835b347b04f5418e515b4a7f49beb74
JP Roemer <jp@roemer.im>
2025-03-22 02:45:08 +0100

    Fix LK23M OLED config and update BootMagic key (#3)
    Update LK23M BootMagic key

--- keyboards/lambdakb/lk23m/config.h
+++ keyboards/lambdakb/lk23m/config.h
@@ -4,6 +4,8 @@
 #pragma once
 
 #define OLED_DISPLAY_128X32
+#define OLED_BRIGHTNESS 48
+#define OLED_UPDATE_INTERVAL 100
 #define OLED_FONT_H "lib/glcdfont.c"
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
