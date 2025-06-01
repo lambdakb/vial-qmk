d2db02afd3b10bcec46688317bca3178009caee3
JP Roemer <jp@roemer.im>
2025-06-02 00:13:53 +0200

    Add LK16C Firmware (#6)

---
+++ keyboards/lambdakb/lk16c/config.h
@@ -0,0 +1,11 @@
+// Copyright 2025 JP Roemer (@0rax)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP2
+#define I2C1_SDL_PIN GP3
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
---
+++ keyboards/lambdakb/lk16c/keymaps/keypad/keymap.c
@@ -0,0 +1,69 @@
+// Copyright 2025 JP Roemer (@0rax)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE
+};
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+     * │ Tab │  Q  │  W  │  E  │  R  │  T  │ F1  │ F2  │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │ Esc │  A  │  S  │  D  │  F  │  G  │ F3  │ F4  │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │Shift│  Z  │  X  │  C  │  V  │  B  │ F5  │ F6  │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │Ctrl │ Alt │ Gui │Lower│Space│Space│Enter│Raise│
+     * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+     */
+    [_QWERTY] = LAYOUT_ortho_4x8(
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_F1  , KC_F2  ,
+        KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_F3  , KC_F4  ,
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_F5  , KC_F6  ,
+        KC_LCTL, KC_LALT, KC_LGUI, LOWER  , KC_SPC , KC_SPC , KC_ENT , RAISE
+    ),
+
+    /*
+     * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+     * │     │     │ Up  │     │     │UGTog│Hue+ │Hue- │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │     │Left │Down │Right│     │     │Val+ │Val- │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │     │     │     │     │     │     │Next │Prev │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │Reset│DbgTg│     │     │     │     │     │     │
+     * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+     */
+    [_LOWER] = LAYOUT_ortho_4x8(
+        _______, XXXXXXX, KC_UP  , XXXXXXX, XXXXXXX, UG_TOGG, UG_HUEU, UG_HUED,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, UG_VALU, UG_VALD,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UG_NEXT, UG_PREV,
+        QK_BOOT, DB_TOGG, _______, _______, _______, _______, _______, _______
+    ),
+
+    /*
+     * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+     * │     │  1  │  2  │  3  │  4  │  5  │ F7  │ F8  │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │     │  6  │  7  │  8  │  9  │  0  │ F9  │ F10 │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │     │     │     │     │     │     │ F11 │ F12 │
+     * ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * │     │     │     │     │     │     │Bspc │ Del │
+     * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+     */
+    [_RAISE] = LAYOUT_ortho_4x8(
+        _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_F7  , KC_F8  ,
+        _______, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_F9  , KC_F10 ,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11 , KC_F12 ,
+        _______, _______, _______, _______, _______, _______, KC_BSPC, KC_DEL
+    ),
+};
